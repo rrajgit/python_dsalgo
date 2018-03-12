01751a9f921645eaa6803353456f33cfa66dc741
dthinkcs <31166087+dthinkcs@users.noreply.github.com>
2018-03-12 14:13:47 +0530

    Update qsort0.py

--- qsort0.py
+++ qsort0.py
@@ -6,17 +6,22 @@ def quicksort(A, left, right):
     quicksort(A, m + 1, right)
     
 def partition(A, left ,right):
+    # pick the leftmost element as the pivot
     pivot = A[left]
     
+    # j is the starting of the second half or part
     j = left
     for i in range(left, right  +1):
+        # element <= pivot then we if j has started then swap
         if A[i] <= pivot:
             if j != left:
                 A[i], A[j] = A[j], A[i]
                 j = j + 1
+        # otherwise if j is still the leftmost thing
         else:
             if j == left:
                 j = i
+    # 3 1 2 vs 1 2 3
     if j == left:
         j = right + 1
     A[left], A[j - 1] = A[j - 1], A[left]
